84df4962487d4ee1fab793b872926dc15f06c2c6
J. Zhao <rho.ajax@gmail.com>
2017-04-06 23:20:54 +0100

    Lexer finished

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -20,6 +20,9 @@ if (5 < 10 ) {
 } else {
   return false;
 }
+
+10 == 10;
+10 != 9;
 `
   tests := []struct {
     expectedType token.TokenType
@@ -90,9 +93,17 @@ if (5 < 10 ) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+    {token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
     {token.EOF, ""},
   }
-  
+
   l := New(input)
 
   for i, tt := range tests {
